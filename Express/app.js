d79b6fb5d3c0c1fadcf34d41a159c9ccf139cccb
Abhinav Jain <66718489+abhinav-22-tech@users.noreply.github.com>
2023-05-17 09:04:26 +0000

    routes

--- Express/app.js
+++ Express/app.js
@@ -3,10 +3,16 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>')
 })
 
 app.listen(3000);
