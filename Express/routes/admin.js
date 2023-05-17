070282e49f2a0e50e839b85c70e67274722d6303
Abhinav Jain <66718489+abhinav-22-tech@users.noreply.github.com>
2023-05-17 10:39:56 +0000

    add some css

--- Express/routes/admin.js
+++ Express/routes/admin.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 router.post('/add-product', (req, res, next) => {
