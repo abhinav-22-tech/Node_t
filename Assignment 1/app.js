90a0638a9082b1e9a7b8ddd5a31b9365032d121a
Abhinav Jain <66718489+abhinav-22-tech@users.noreply.github.com>
2023-05-18 10:15:55 +0000

    Assignment 1

--- Assignment 1/app.js
+++ Assignment 1/app.js
@@ -8,7 +8,10 @@ const app = express();
 const roots = require('./routes/root');
 const users = require('./routes/users');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(roots);
 app.use(users);
+
 
 app.listen(3000);
--- Assignment 1/routes/root.js
+++ Assignment 1/routes/root.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const path = require('path');
+
 const router = express.Router(); 
 
 router.get('/', (req, res, next) => {
-    res.send('Hello');
+    res.sendFile(path.join(__dirname, '../', 'views', 'main.html'));
 })
 
 module.exports = router;
