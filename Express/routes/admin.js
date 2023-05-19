fe8239f82acca6124c02dd241b2d1820e8e4efeb
Abhinav Jain <66718489+abhinav-22-tech@users.noreply.github.com>
2023-05-19 13:26:01 +0000

    clean up

--- Express/routes/admin.js
+++ Express/routes/admin.js
@@ -13,7 +13,7 @@ router.get('/add-product', (req, res, next) => {
 })
 
 router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
+    products.push({ title: req.body.title });
     res.redirect('/');
 })
 
