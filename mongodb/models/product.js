f840e905d27677e697d67ae706d00e4d1ebc8556
Abhinav Jain <66718489+abhinav-22-tech@users.noreply.github.com>
2023-05-26 15:25:57 +0000

    CRD

--- mongodb/models/product.js
+++ mongodb/models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -11,12 +12,65 @@ class Product {
   save() {
     const db = getDb();
     return db.collection('products').insertOne(this)
-    .then(result => {
-      console.log(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products').find().toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static updateById(prodId, ttle, prce, imgUrl, desc) {
+    const db = getDb();
+    return db.collection('products')
+      .update({ _id: new mongodb.ObjectId(prodId) },
+        { $set: { title: ttle, price: prce, imageUrl: imgUrl, description: desc } })
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
+
+  static destroy(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err)
+      });
   }
 }
 
