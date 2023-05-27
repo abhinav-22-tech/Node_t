c33bf96ec4a9ba7e540b4f37a6d7807064962939
Abhinav Jain <66718489+abhinav-22-tech@users.noreply.github.com>
2023-05-27 06:29:34 +0000

    CRUD

--- mongodb/models/product.js
+++ mongodb/models/product.js
@@ -2,16 +2,24 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db.collection('products').insertOne(this)
+    let dbOps;
+    if (this._id) {
+      dbOps = db.collection('products').updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    }
+    else {
+      dbOps = db.collection('products').insertOne(this)
+    }
+    return dbOps
       .then(result => {
         console.log(result);
       })
@@ -46,19 +54,19 @@ class Product {
       });
   }
 
-  static updateById(prodId, ttle, prce, imgUrl, desc) {
-    const db = getDb();
-    return db.collection('products')
-      .update({ _id: new mongodb.ObjectId(prodId) },
-        { $set: { title: ttle, price: prce, imageUrl: imgUrl, description: desc } })
-      .then(product => {
-        console.log(product);
-        return product;
-      })
-      .catch(err => {
-        console.log(err)
-      });
-  }
+  // static updateById(prodId, ttle, prce, imgUrl, desc) {
+  // const db = getDb();
+  // return db.collection('products')
+  // .update({ _id: new mongodb.ObjectId(prodId) },
+  // { $set: { title: ttle, price: prce, imageUrl: imgUrl, description: desc } })
+  // .then(product => {
+  // console.log(product);
+  // return product;
+  // })
+  // .catch(err => {
+  // console.log(err)
+  // });
+  // }
 
   static destroy(prodId) {
     const db = getDb();
